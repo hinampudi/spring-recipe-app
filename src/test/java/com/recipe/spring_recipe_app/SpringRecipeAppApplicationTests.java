db8cac1fb9ddf86905c524439356925f13b69897
hinampudi <hinampudi2021@my.fit.edu>
2023-01-26 12:56:57 -0600

    Git Initial Commit

---
+++ src/test/java/com/recipe/spring_recipe_app/SpringRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipe.spring_recipe_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
